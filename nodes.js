8b6687ae64f10c9ca2e62eed0bcc6ad8229c6d40
Tamara Reyda <tamara.reyda@gmail.com>
2014-04-03 01:35:48 -0700

    changed node visualization to use HTML and CSS transforms instead of HTML5 canvas, updated math for node visualization

--- nodes.js
+++ nodes.js
@@ -1,3 +1,4 @@
+//put list of token strings here:
 var tokenArray = ["250000",
 "100000000000000000000000000000000000000","85000000000000000000000000000000000000",
 "45000000000000000000000000000000000000","20000000000000000000000000000000000000",
@@ -5,34 +6,24 @@ var tokenArray = ["250000",
 "170000000000000000000000000000000000000"]; //can be up to 100 nodes
 var arrayLength = tokenArray.length;
 
-//draw ring visualization
-var ringCanvas = document.getElementById("ring");
-var ringContext = ringCanvas.getContext("2d");
-ringContext.beginPath();
-ringContext.arc(150, 150, 100, 0, Math.PI*2, false);
-ringContext.closePath();
-ringContext.fillStyle = "#383838";
-ringContext.fill();
-
 //add nodes to ring
 for (i=0;i<arrayLength;i++) {
-	var angle = tokenMath(tokenArray[i]);
+	var token = tokenArray[i];
+	var angle = estimateAngleFromToken(token);
 	var x = getNodeXCoordinate(angle);
 	var y = getNodeYCoordinate(angle);
-	drawNode(x, y);
+	drawNode(token, x, y);
 }
 
 //function that deals with big number tokens
-function tokenMath(token){
+function estimateAngleFromToken(token){
 	var tokenLength = token.length;
-	console.log(tokenLength);
+	//console.log(tokenLength);
+	var truncToken = 0;
 	if (tokenLength > 36) {
 		var truncTokenString = token.slice(0,tokenLength-36);
-		var truncToken = Number(truncTokenString);
-		console.log(truncToken);
-	}
-	else {
-		var truncToken = 0;
+		truncToken = Number(truncTokenString);
+		//console.log(truncToken);
 	}
 	var angle1 = Math.PI*2*truncToken/170 //170 is first 3 digits of 2^127
 	var angle2 = Math.PI/2-angle1;
@@ -42,14 +33,35 @@ function tokenMath(token){
 //function that uses the token to determine where to place the node on the ring (x)
 function getNodeXCoordinate(angle) {
 	var x1 = 100*Math.cos(angle) //radius of ring is 100 in this case
-	return x1+150; //accounts for canvas coordinate plane
+	return x1+100; //accounts for coordinate plane translation & radius (10) of node
 }
 
 //function that uses the token to determine where to place the node on the ring (y)
 function getNodeYCoordinate(angle) {
 	var y1 = 100*Math.sin(angle) //radius of ring is 100 in this case
-	return 150-y1; //accounts for canvas coordinate plane
+	return 100-y1; //accounts for coordinate plane translation & radius (10) of node
 }
+
+//function that draws the node on the ring
+function drawNode(token, x, y) {
+	var node = document.createElement("div");
+	node.className = "node";
+	node.id = token;
+	node.addEventListener("click", function(){console.log("Token "+node.id)}, false);
+	node.style.WebkitTransform = "translate("+x+"px, "+y+"px)"; //Chrome, Opera, Safari
+	//node.style.transform = "translate("+x+"px, "+y+"px)";
+	document.getElementById("container").appendChild(node);
+}
+
+/* HTML5 CANVAS
+//draw ring visualization
+var ringCanvas = document.getElementById("ring");
+var ringContext = ringCanvas.getContext("2d");
+ringContext.beginPath();
+ringContext.arc(150, 150, 100, 0, Math.PI*2, false);
+ringContext.closePath();
+ringContext.fillStyle = "#383838";
+ringContext.fill();
 
 //function that draws the node on the ring
 function drawNode(x, y) {
@@ -59,3 +71,4 @@ function drawNode(x, y) {
 	ringContext.fillStyle = "#009966";
 	ringContext.fill();
 }
+*/
